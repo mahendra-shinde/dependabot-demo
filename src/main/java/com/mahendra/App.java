4d321967549686f6b71f95a3a1de0a1e90d888d6
Mahendra Shinde <MahendraShinde@synergetics-india.com>
2022-08-23 18:02:02 +0530

    Made few changes

--- src/main/java/com/mahendra/App.java
+++ src/main/java/com/mahendra/App.java
@@ -9,6 +9,6 @@ public class App
     private static final String message = "Hello WOrld";
     public static void main( String[] args )
     {
-        System.out.println(  App.message);
+        System.out.println(  new App().message );
     }
 }
